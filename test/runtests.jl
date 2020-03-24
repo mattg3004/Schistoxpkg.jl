0743e4a7e98fc92bd32d9e097ef1dd262116b948
Matthew <matthewgraham3004@gmail.com>
2020-03-24 17:25:26 +0800

    add some more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -49,13 +49,35 @@ end
     @test death_rate < death_rate_2
 end
 
+@testset "update_contact_rate" begin
+    @test isapprox(update_contact_rate([5,10,3], [0,0,0], [1,2,3,4,5,6,7,8,9,10,11,12]), [6,11,4])
+end
 
+
+
+
+
+@testset "update_death_rate" begin
+    @test isapprox(update_death_rate([0,3,6,50, 1000], [0,0,0,0,0], [1,2,3,4,5,6,7,8,9,10,11,12]), [1,2,3,11,12])
+end
+
+
+
+@testset "cercariae_death" begin
+    @test cercariae_death(1000,1,1) == 500
+end
+
+
+@testset "worm_pairs" begin
+    @test isapprox(calculate_worm_pairs([[1,3],[17,0],[400,12312]],[[100,3000],[1,6],[1,1]]),[4,7,2] )
+end
 
 
 
 @testset "make_age_contact_rate_array(max_age)" begin
     @test make_age_contact_rate_array(100)[1] == 0.032
 end
+
 
 
 
