49c76508c56d2277af1402b025317eb78768c4cc
Matthew <matthewgraham3004@gmail.com>
2020-04-02 11:13:43 +0800

    update some test

--- test/runtests.jl
+++ test/runtests.jl
@@ -55,12 +55,9 @@ end
 
 
 
-
-
-@testset "update_death_rate" begin
-    @test isapprox(update_death_rate([0,3,6,50, 1000], [0,0,0,0,0], [1,2,3,4,5,6,7,8,9,10,11,12]), [1,2,3,11,12])
+@testset "miracidia_death" begin
+    @test miracidia_death([1000,1421,43523,55],1) == [1000,1421,43523,28]
 end
-
 
 
 @testset "cercariae_death" begin
