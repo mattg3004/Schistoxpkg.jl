ae36ec7cc7f74c6d7c65bb7d6c2e86955282f885
Matthew <matthewgraham3004@gmail.com>
2020-07-30 14:31:37 +0800

    add comments

--- src/example_run.jl
+++ src/example_run.jl
@@ -4,10 +4,14 @@ using JLD
 using Distributions
 using PyPlot
 
+# include parameters and add a file name
 include("parameters.jl")
 filename = "equ.jld"
 
+# make contact rates by age array
 contact_rates_by_age = make_age_contact_rate_array(max_age, scenario, [], []);
+
+# create population
 ages , death_ages, gender, predisposition, community,
    human_cercariae, eggs, vac_status,
    treated, female_worms, male_worms, age_contact_rate,
@@ -18,13 +22,16 @@ ages , death_ages, gender, predisposition, community,
                 spec_ages, ages_per_index, death_prob_by_age, ages_for_deaths,
                 mda_adherence, mda_access)
 
+# add death ages for each person
 death_ages = []
 for i in 1:N
     push!(death_ages, get_death_age(death_prob_by_age, ages_for_deaths))
 end
-mean(death_ages)
+
+# make it so that no ones age is greater than their deatha age
 ages, death_ages = generate_ages_and_deaths(20000, ages, death_ages, death_prob_by_age, ages_for_deaths)
 
+# run to equilibrium for time length defined in parameters.jl
 ages_equ, gender_equ, predisposition_equ,  human_cercariae_equ, eggs_equ,
 vac_status_equ, treated_equ, female_worms_equ, male_worms_equ,
 vaccinated_equ, env_miracidia_equ, env_cercariae_equ, record_high =
@@ -40,7 +47,7 @@ vaccinated_equ, env_miracidia_equ, env_cercariae_equ, record_high =
         miracidia_maturity_time)
 
 
-
+# save variables to file
 save(filename, "ages", ages_equ ,  "gender", gender_equ,"predisposition",   predisposition_equ,
             "human_cercariae", human_cercariae_equ, "eggs", eggs_equ, "community", community,
             "vac_status", vac_status_equ,"treated", treated_equ, "female_worms",  female_worms_equ, "male_worms", male_worms_equ,
@@ -48,19 +55,21 @@ save(filename, "ages", ages_equ ,  "gender", gender_equ,"predisposition",   pred
             "env_miracidia",env_miracidia_equ, "env_cercariae", env_cercariae_equ, "adherence", adherence, "access", access)
 
 
-        ###### run baseline case where mda isn't missed ########
+###### run baseline case where mda isn't missed ########
 
+# decide how long to run mda experiments for, along with how many times to run for
 number_years_mda = 20
 num_time_steps = trunc(Int, 365*number_years_mda / time_step)
+num_repeats = 10
 
+# create mda information
 mda_info = create_mda(0, .75, 0, 1, number_years_mda, 1, [0,1], [0,1], [0,1], drug_efficacy)
 
 vaccine_info = []
 
-num_repeats = 10
 
-
-        times, prev, sac_prev, high_burden, high_burden_sac, adult_prev = run_repeated_sims_no_population_change(num_repeats, num_time_steps,
+# run simulations for mda analysis
+times, prev, sac_prev, high_burden, high_burden_sac, adult_prev = run_repeated_sims_no_population_change(num_repeats, num_time_steps,
             time_step, average_worm_lifespan,community_contact_rate, community_probs,
             max_fecundity, r, worm_stages, predis_aggregation, predis_weight,vaccine_effectiveness,
             density_dependent_fecundity, contact_rate, env_cercariae_survival_prop, env_miracidia_survival_prop,
@@ -68,12 +77,14 @@ num_repeats = 10
             death_prob_by_age, ages_for_deaths, birth_rate, mda_info, vaccine_info, mda_adherence, mda_access,
             record_frequency, filename, human_cercariae_prop, miracidia_maturity_time)
 
-        mean_prev_baseline = mean.(prev)
-        mean_sac_prev_baseline = mean.(sac_prev)
-        mean_high_burden_baseline = mean.(high_burden)
-        mean_high_burden_sac_baseline = mean.(high_burden_sac)
-        mean_adult_prev_baseline = mean.(adult_prev)
+# generate variables to plot
+mean_prev_baseline = mean.(prev)
+mean_sac_prev_baseline = mean.(sac_prev)
+mean_high_burden_baseline = mean.(high_burden)
+mean_high_burden_sac_baseline = mean.(high_burden_sac)
+mean_adult_prev_baseline = mean.(adult_prev)
 
+# clear figure and plot
 clf()
-        plt.plot(times, mean_sac_prev_baseline)
-        gcf()
+plt.plot(times, mean_sac_prev_baseline)
+gcf()
